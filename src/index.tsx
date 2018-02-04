25a07fb0adda5f51fad0135eb9aae055071ecfaa
Thomas Hermann <novascreen@gmail.com>
2018-02-04 18:00:15 -0500

    Add basic HMR support

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,21 @@ import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const rootEl = document.getElementById('root') as HTMLElement;
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root') as HTMLElement
+  rootEl
 );
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
+
 registerServiceWorker();
