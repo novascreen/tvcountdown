a7dfab1ac9623930a2ab1695a2d0e040234dcd1e
Thomas Hermann <novascreen@gmail.com>
2018-02-07 22:45:19 -0500

    Update model types

--- src/models/graphql.ts
+++ src/models/graphql.ts
@@ -1,13 +1,19 @@
+export type Image = {
+  medium?: string
+  original?: string
+};
+
 export type Show = {
-  id: number;
-  name: string;
+  id: number
+  name: string
+  image?: Image
 };
 
 export type Episode = {
-  id: number;
-  name: string;
-  season?: number;
-  number?: number;
-  airstamp: string;
-  show: Show;
+  id: number
+  name: string
+  season?: number
+  number?: number
+  airstamp: string
+  show: Show
 };
