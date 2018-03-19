2978d159118d38f3852579fdbe8fc805a916a05e
Thomas Hermann <novascreen@gmail.com>
2018-03-18 22:30:46 -0400

    Fix error after logging in

--- src/api/withMyFavoriteShows.ts
+++ src/api/withMyFavoriteShows.ts
@@ -1,5 +1,7 @@
-import { graphql, QueryProps } from 'react-apollo';
+import { graphql, QueryProps, compose } from 'react-apollo';
 import gql from 'graphql-tag';
+import { withProps } from 'recompose';
+
 import { isAuthenticated } from 'lib/Auth';
 import fragments from './fragments';
 
@@ -12,15 +14,15 @@ const GET_MY_FAVORITE_SHOWS = gql`
   ${fragments.myFavoriteShows}
 `;
 
-const withMyFavoriteShows = graphql<QueryProps, any, Response>(
-  GET_MY_FAVORITE_SHOWS,
-  {
-    skip: () => !isAuthenticated(),
+const withMyFavoriteShows = compose(
+  withProps(() => ({ auth: isAuthenticated() })),
+  graphql<QueryProps, any, Response>(GET_MY_FAVORITE_SHOWS, {
+    skip: ({ auth }) => !auth,
     props: ({ data }) => ({
       ...data,
       loadingMyFavoriteShows: data && data.loading,
     }),
-  },
+  }),
 );
 
 export default withMyFavoriteShows;
