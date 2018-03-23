e20ab70a36bd21104755afbcc9d694da6bf444cc
Thomas Hermann <novascreen@gmail.com>
2018-03-22 22:39:59 -0400

    Track Countdown state in URL

--- src/components/Pages/Countdown/index.tsx
+++ src/components/Pages/Countdown/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Fragment } from 'react';
+import { RouteComponentProps } from 'react-router';
+import * as qs from 'query-string';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -11,23 +13,29 @@ import FavoritesSchedule from 'components/Schedule/FavoritesSchedule';
 import TimeSelector from 'components/Schedule/TimeSelector';
 import Divider from 'material-ui/Divider';
 
-export class CountdownPage extends React.Component {
-  state = {
-    shows: 'all',
-    time: 'upcoming',
-  };
+export class CountdownPage extends React.Component<RouteComponentProps<{}>> {
+  get query() {
+    return {
+      shows: 'all',
+      time: 'upcoming',
+      ...qs.parse(this.props.location.search),
+    };
+  }
 
   handleShowsChange = (e: any, value: string) => {
-    this.setState({ shows: value });
+    const { history } = this.props;
+    history.push(`/?shows=${value}`);
   };
 
   handleTimeChange = (e: any) => {
-    this.setState({ time: e.target.value });
+    const { history } = this.props;
+    history.push(`/?shows=${this.query.shows}&time=${e.target.value}`);
   };
 
   render() {
-    const { shows, time } = this.state;
+    const { shows, time } = this.query;
     const previous = time === 'previous';
+
     return (
       <Fragment>
         <Box mB={2}>
