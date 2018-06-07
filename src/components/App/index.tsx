078e9b03340d163306682d1dfef8656d74758c81
Thomas Hermann <novascreen@gmail.com>
2018-06-06 22:45:37 -0400

    Fix login for Android Web APK The authentication popup is not really a popup, so it doesn't close. In that case we simply login in the "popup" and display redirect to the home page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -12,17 +12,28 @@ const handleAuthentication = ({ location }: any) => {
   }
 };
 
+class Popup extends React.Component {
+  componentDidMount() {
+    // Closes popup if possible
+    auth.auth0.popup.callback();
+
+    // In some scenarios like Android Web APK (Installed to Homescreen)
+    // the popup can't be close
+    // In this case we handle authentication in the current window
+    setTimeout(() => {
+      handleAuthentication({ location: window.location });
+    }, 100);
+  }
+
+  render() {
+    return null;
+  }
+}
+
 export const AppContainer = () => (
   <Router history={appHistory}>
     <Switch>
-      <Route
-        exact
-        path="/auth/popup"
-        render={props => {
-          auth.auth0.popup.callback();
-          return <Loading />;
-        }}
-      />
+      <Route exact path="/auth/popup" component={Popup} />
       <Route
         exact
         path="/auth/callback"
--- src/lib/Auth.ts
+++ src/lib/Auth.ts
@@ -88,6 +88,7 @@ export default class Auth {
 
   handleAuthentication = () => {
     this.auth0.parseHash((err: any, authResult: any) => {
+      console.log(authResult);
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
       } else if (err) {
