b0147935bf41364d597c5e428c1076dacb8376e4
Thomas <thomas.h@ecobee.com>
2019-11-08 00:02:47 -0500

    Update prisma config and define schema in TS

--- server/index.ts
+++ server/index.ts
@@ -1,60 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-
 import { ApolloServer, AuthenticationError } from 'apollo-server-micro';
 import { makeExecutableSchema } from 'graphql-tools';
-import { importSchema } from 'graphql-import';
-import jwt from 'jsonwebtoken';
-import jwksClient from 'jwks-rsa';
 
 import { Prisma } from './generated/prisma-client';
 import resolvers from './resolvers';
 import directiveResolvers from './directives';
-import { AUTH_CONFIG } from './serverConstants';
-import { Context, Auth0User } from 'utils/utils';
-import JwksRsa from 'jwks-rsa';
+import { Context } from 'utils/utils';
 import { getAuth0User } from './utils/getAuth0User';
-
-// Ensure the file is included in ncc build
-fs.readFileSync(path.join(__dirname, '/prisma.graphql'));
+import { typeDefs } from './typeDefs';
 
 const isProduction = process.env.NODE_ENV === 'production';
-
-const client = jwksClient({
-  cache: true,
-  rateLimit: true,
-  jwksRequestsPerMinute: 1,
-  jwksUri: `https://${AUTH_CONFIG.domain}/.well-known/jwks.json`,
-});
-
-type GetKey = (
-  header: { typ: string; alg: string; kid: string },
-  cb: Function,
-) => void;
-const getKey = (header: any, cb: Function) => {
-  client.getSigningKey(header.kid, function(error, key: any) {
-    if (error) cb(error);
-    else {
-      var signingKey = key.publicKey || key.rsaPublicKey;
-      cb(null, signingKey);
-    }
-  });
-};
-
-const options = {
-  secret: JwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 1,
-    jwksUri: `https://${AUTH_CONFIG.domain}/.well-known/jwks.json`,
-  }),
-
-  // Validate the audience and the issuer.
-  credentialsRequired: false,
-  audience: AUTH_CONFIG.audience,
-  issuer: `https://${AUTH_CONFIG.domain}/`,
-  algorithms: ['RS256'],
-};
 
 const db = new Prisma({
   // the endpoint of the Prisma DB service (value is set in .env)
@@ -66,7 +20,7 @@ const db = new Prisma({
 });
 
 const schema = makeExecutableSchema({
-  typeDefs: importSchema(path.join(__dirname, '/schema.graphql')),
+  typeDefs,
   resolvers,
   directiveResolvers,
 } as any);
